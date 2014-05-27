47b83f13eaef656e78486776c44457ac42446ab7
Jake Rosoman <jkroso@gmail.com>
2014-05-28 00:04:51 +1200

    support hydro 0.10

--- index.js
+++ index.js
@@ -5,50 +5,24 @@
 var Formatter = require('hydro-formatter');
 
 /**
- * Dot formatter.
- *
- * @constructor
- */
-
-var Dot = Formatter.extend();
-
-/**
- * Before all tests.
- *
- * @api public
- */
-
-Dot.prototype.beforeAll = function() {
-  this.println();
-  this.print(this.padding);
-};
-
-/**
- * After each test.
- *
- * @param {Object} test
- * @api public
- */
-
-Dot.prototype.afterTest = function(test) {
-  if (test.status === 'skipped') return;
-  this.print(this.color('.', this.statusColor[test.status]));
-};
-
-/**
- * After all tests.
- *
- * @api public
- */
-
-Dot.prototype.afterAll = function() {
-  this.println();
-  this.displayResult();
-  this.displayFailed();
-};
-
-/**
  * Primary export.
  */
 
-module.exports = Dot;
+module.exports = function(hydro){
+  var dot = new Formatter;
+  dot.beforeAll = function() {
+    this.println();
+    this.print(this.padding);
+  };
+  dot.afterTest = function(test) {
+    if (test.status === 'skipped') return;
+    this.print(this.color('.', this.statusColor[test.status]));
+  };
+  dot.afterAll = function() {
+    this.println();
+    this.displayResult();
+    this.displayFailed();
+  };
+  dot.use(hydro);
+  return dot;
+};
--- test.js
+++ test.js
@@ -1,12 +1,16 @@
 
-var Formatter = require('./');
-
-var formatter = new Formatter;
+var Hydro = require('hydro');
+var formatter = require('./')(new Hydro);
 
 formatter.beforeAll();
 
 for (var i = 1; i < 51; i++) {
-  var test = {time:1, title: 'test ' + i, error: new Error('boom')};
+  var test = {
+    time:1,
+    title: 'test ' + i,
+    error: new Error('boom'),
+    fullTitle: function(){ return this.title; }
+  };
   var n = Math.random();
   if (n > .99) test.status = 'pending';
   else if (n > .98) test.status = 'skipped';
